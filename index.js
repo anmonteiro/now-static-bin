dcd46e8ebd4c67bd9a93dac8f45a5c77c4a937fb
António Nuno Monteiro <anmonteiro@gmail.com>
2018-11-23 21:59:44 +0000

    Add a simple analyze step (#8)
    This is related to the problem I was seeing in #6. See [#6
    (comment)](https://github.com/anmonteiro/now-static-bin/pull/6#issuecomment-441272828).
    I think just returning the digest of the binary might be enough but let's
    also include the hash of the configuration object for now, given that
    the timeout could change between deployments, even if the binary 
    doesn't.
    
    Still waiting to hear more regarding the `config` object from the Zeit
    team.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const crypto = require('crypto');
+const path = require('path');
 const { createLambda } = require('@now/build-utils/lambda.js');
 const glob = require('@now/build-utils/fs/glob.js');
-const path = require('path');
 const rename = require('@now/build-utils/fs/rename.js');
+const objectHash = require('object-hash');
 
 exports.config = {
   maxLambdaSize: '25mb',
+};
+
+exports.analyze = ({ files, entrypoint, config }) => {
+  const entrypointHash = files[entrypoint].digest;
+  const objHash = objectHash(config, { algorithm: 'sha256' });
+  const combinedHashes = [entrypointHash, objHash].join('');
+
+  return crypto
+    .createHash('sha256')
+    .update(combinedHashes)
+    .digest('hex');
 };
 
 exports.build = async ({ files, entrypoint, config }) => {
