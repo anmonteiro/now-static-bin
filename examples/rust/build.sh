3260ce6f21c46ff375a7842d76fd43e835cfebce
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-11-23 22:43:56 +0000

    better

--- examples/rust/build.sh
+++ examples/rust/build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
+set -eo pipefail
+
+root_path=$PWD
+
+# Start in examples/rust/ even if run from root directory
+cd "$(dirname "$0")"
+
 docker build . --tag rust-example
+docker rm rust-example || true
 docker create --name rust-example rust-example
-docker cp rust:/server.exe .
+docker cp rust-example:/server.exe .
+
+cd $root_path
