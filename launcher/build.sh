98bfc53fd4f9b1309ebefe1507d024d39491460d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-11-23 20:31:56 +0000

    pipe stdout / stderr from child process

---
+++ launcher/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e pipefail
+
+root_path=$PWD
+
+# Start in launcher/ even if run from root directory
+cd "$(dirname "$0")"
+
+docker build . --tag now-static-bin-launcher
+docker rm now-static-bin-launcher || true
+docker create --name now-static-bin-launcher now-static-bin-launcher
+docker cp now-static-bin-launcher:/root/go/app/launcher ../dist
+
+cd $root_path
